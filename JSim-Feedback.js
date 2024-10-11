1a556994475f56dd42a972eeff97388c7a8afd69
Fute <leonardoduarte2744@gmail.com>
2024-10-11 08:10:28 -0300

    Delete JSim-Feedback.js

--- JSim-Feedback.js
+++
@@ -1,5 +0,0 @@
-var review = document.getElementById('txtRevi').value;
-
-function Enviar(){
-    alert("cu do jandersinha eh alargado, não faz diferença pro meu pau. Tenho que comer o cu de outros para me saisfazer, pois o cu do jandersinha não me satisfaz ao botar minha piroca");
-}
