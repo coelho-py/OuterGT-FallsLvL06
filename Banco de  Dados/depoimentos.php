ee37a7ff3a616d67facf5dc3e37a5572719f3275
Futeboli <leonardoduarte2744@gmail.com>
2024-10-24 10:30:11 -0300

    att referente a feedbacks

--- Banco de  Dados/depoimentos.php
+++ Banco de  Dados/depoimentos.php
@@ -1,7 +1,26 @@
 <?php
 
+require("conector.php");
+
 if (isset($_POST)) {
     echo "<pre>";
     print_r($_POST);
     echo "</pre>";
+
+    // variaveis do html dos quais sao escritas
+    $nomeUsuario = $_POST['nomeUsuario'];
+    $serieUsuario = $_POST['serieUsuario'];
+    $classeUsuario = $_POST['classeUsuario'];
+    $depoimento = $_POST['textoReview'];
+
+    // querys para mandar o que o usuario escreveu par ao banco de dados
+
+    $query = "INSERT INTO usuario(nomeUsuario, serieUsuario, classeUsuario) VALUES ('$nomeUsuario', '$serieUsuario', '$classeUsuario')";
+
+    $query1 = "INSERT INTO depoimento(review) VALUES ('$depoimeto')";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+
+    header("Location: index.php");
 }
